83522271c65eb80bb091f8e13fc7355437ed9fb8
kien van ba <kien.vanba@gmail.com>
2019-07-24 11:53:20 +0700

    setup android with codepush

--- android/app/src/main/java/com/pcodepush/MainApplication.java
+++ android/app/src/main/java/com/pcodepush/MainApplication.java
@@ -7,9 +7,12 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
 
 import java.util.Arrays;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -19,10 +22,17 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
+    @Nullable
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new CodePush("NRGvw-dnnyy-BxeuJuDw0dU5HtatUfOoIWlje", MainApplication.this, BuildConfig.DEBUG)
       );
     }
 
