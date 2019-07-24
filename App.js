67e5019655d9644e0280f29d4b0707f64c25aece
kien van ba <kien.vanba@gmail.com>
2019-07-24 14:18:07 +0700

    test codepush

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
   StyleSheet,
 } from 'react-native';
 import CodePush from 'react-native-code-push';
@@ -10,11 +11,32 @@ const codePushOption = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
 }
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Hello CodePush!</Text>
-  </View>
-);
+const App = () => {
+  const [logs, setLogs] = useState([`sync started at: ${new Date().getTime()}`]);
+
+  function codePushSync() {
+    CodePush.sync({
+      updateDialog: true,
+      installMode: CodePush.InstallMode.IMMEDIATE,
+    }, status => {
+      for (var key in CodePush.SyncStatus) {
+        if (status === CodePush.SyncStatus[key]) {
+          setLogs([...logs, key.replace(/_/g, '')]);
+          break;
+        }
+      }
+    });
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello CodePush ver1.0.1!</Text>
+      <TouchableOpacity style={styles.button} onPress={() => codePushSync()}>
+        <Text>Code Push</Text>
+      </TouchableOpacity>
+      <Text>{JSON.stringify(logs)}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -25,6 +47,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 32,
+  },
+  button: {
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
